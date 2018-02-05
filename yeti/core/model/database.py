c8936751402fb84a89f38ab1c9e17a7ccec99482
Thomas Chopitea <tomchop@gmail.com>
2018-02-05 23:44:54 +0100

    Remove useless code

--- yeti/core/model/database.py
+++ yeti/core/model/database.py
@@ -15,16 +15,6 @@ class YetiObject(ArangoYetiConnector):
     _schema = None
     _indexes = []
 
-    @classmethod
-    def collection_name(cls):
-        """Return the collection or table name corresponding to this class.
-
-        Returns;
-          A string representing the collection name.
-        """
-        return cls._collection_name or cls.__name__.lower()
-
-
 class YetiSchema(ArangoYetiSchema):
     """Generic (de)serialization marshmallow.Schema object for Yeti objects."""
     pass
