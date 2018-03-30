f128fc2fbde028507250627e1a6115e01a227e32
Thomas Chopitea <tomchop@gmail.com>
2018-03-30 21:06:39 +0200

    Initial vue.js setup

---
+++ yeti/web/frontend/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const routerOptions = [
+  { path: '/', component: 'Home' },
+  { path: '/observables', component: 'Observables' },
+  { path: '*', component: 'NotFound' }
+]
+const routes = routerOptions.map(route => {
+  return {
+    ...route,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
+
+Vue.use(Router)
+
+export default new Router({
+  routes,
+  mode: 'history'
+})
