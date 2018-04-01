6d7441784c700262fadd8f39f4036d4cabeac5f4
Thomas Chopitea <tomchop@gmail.com>
2018-04-01 21:04:12 +0200

    Extract Sidebar

--- yeti/web/frontend/src/App.vue
+++ yeti/web/frontend/src/App.vue
@@ -16,42 +16,7 @@
     <div class="container-fluid">
       <div class="row">
         <!-- sidebar column -->
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <router-link class="nav-link" to="/observables">
-                  <span data-feather="home"></span>Observables
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link class="nav-link" to="/entities">
-                  <span data-feather="file"></span>Entities
-                </router-link>
-              </li>
-              <li class="nav-item">
-                <router-link class="nav-link" to="/indicators">
-                  <span data-feather="file"></span>Indicators
-                </router-link>
-              </li>
-            </ul>
-
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-              <span>Advanced</span>
-              <a class="d-flex align-items-center text-muted" href="#">
-                <span data-feather="plus-circle"></span>
-              </a>
-            </h6>
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Browse
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <sidebar/>
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
           <router-view :searchQuery='searchQuery'/>
         </main>
@@ -66,8 +31,12 @@
 <script>
 require('bootstrap')
 require('bootstrap/dist/css/bootstrap.css')
+import Sidebar from '@/components/scaffolding/Sidebar'
 
 export default {
+  components: {
+    Sidebar
+  },
   data () {
     return {
       searchQuery: ''
---
+++ yeti/web/frontend/src/components/scaffolding/Sidebar.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+    <div class="sidebar-sticky">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <router-link class="nav-link" to="/observables">
+            <span data-feather="home"></span>Observables
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/entities">
+            <span data-feather="file"></span>Entities
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/indicators">
+            <span data-feather="file"></span>Indicators
+          </router-link>
+        </li>
+      </ul>
+
+      <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+        <span>Advanced</span>
+        <a class="d-flex align-items-center text-muted" href="#">
+          <span data-feather="plus-circle"></span>
+        </a>
+      </h6>
+      <ul class="nav flex-column mb-2">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span data-feather="file-text"></span>
+            Browse
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
