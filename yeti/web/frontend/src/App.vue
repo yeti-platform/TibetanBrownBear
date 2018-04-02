de610bcb9a132180e89440134164ef222dd02b54
Thomas Chopitea <tomchop@gmail.com>
2018-04-02 18:46:23 +0200

    Scaffolding edits

--- yeti/web/frontend/src/App.vue
+++ yeti/web/frontend/src/App.vue
@@ -3,22 +3,14 @@
   <div id="app">
 
     <!-- Top menu -->
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Yeti</a>
-      <input v-model="searchQuery" class="form-control form-control-dark w-100" type="text" placeholder="Search query" aria-label="Search">
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
-      </ul>
-    </nav>
+    <navigation/>
 
     <div class="container-fluid">
       <div class="row">
         <!-- sidebar column -->
         <sidebar/>
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-          <router-view :searchQuery='searchQuery'/>
+          <router-view/>
         </main>
 
       </div> <!-- end row -->
@@ -30,18 +22,15 @@
 
 <script>
 import Sidebar from '@/components/scaffolding/Sidebar'
+import Navigation from '@/components/scaffolding/Navigation'
 
 require('bootstrap')
 require('bootstrap/dist/css/bootstrap.css')
 
 export default {
   components: {
-    Sidebar
-  },
-  data () {
-    return {
-      searchQuery: ''
-    }
+    Sidebar,
+    Navigation
   },
   name: 'App',
   metaInfo: {
@@ -68,87 +57,6 @@ body {
   height: 16px;
   vertical-align: text-bottom;
 }
-
-/*
- * Sidebar
- */
-
-.sidebar {
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 100; /* Behind the navbar */
-  padding: 0;
-  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .1);
-}
-
-.sidebar-sticky {
-  position: -webkit-sticky;
-  position: sticky;
-  top: 48px; /* Height of navbar */
-  height: calc(100vh - 48px);
-  padding-top: .5rem;
-  overflow-x: hidden;
-  overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
-}
-
-.sidebar .nav-link {
-  font-weight: 500;
-  color: #333;
-}
-
-.sidebar .nav-link .feather {
-  margin-right: 4px;
-  color: #999;
-}
-
-.sidebar .nav-link.router-link-active {
-  color: #007bff;
-}
-
-.sidebar .nav-link:hover .feather,
-.sidebar .nav-link.router-link-active .feather {
-  color: inherit;
-}
-
-.sidebar-heading {
-  font-size: .75rem;
-  text-transform: uppercase;
-}
-
-/*
- * Navbar
- */
-
-.navbar-brand {
-  padding-top: .75rem;
-  padding-bottom: .75rem;
-  font-size: 1rem;
-  background-color: rgba(0, 0, 0, .25);
-  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
-}
-
-.navbar .form-control {
-  padding: .75rem 1rem;
-  border-width: 0;
-  border-radius: 0;
-}
-
-.form-control-dark {
-  color: #fff;
-  background-color: rgba(255, 255, 255, .1);
-  border-color: rgba(255, 255, 255, .1);
-}
-
-.form-control-dark:focus {
-  border-color: transparent;
-  box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
-}
-
-/*
- * Utilities
- */
 
 .border-top { border-top: 1px solid #e5e5e5; }
 .border-bottom { border-bottom: 1px solid #e5e5e5; }
---
+++ yeti/web/frontend/src/components/scaffolding/Navigation.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <nav class="navbar navbar-expand-md navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Yeti</a>
+    <div class="navbar-collapse" id="navbarCollapse">
+    <ul class="navbar-nav px-3 ml-auto">
+      <li class="nav-item">
+          <a class="nav-link active" href="#">Settings</a>
+      </li>
+    </ul>
+  </div>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+
+.navbar-brand {
+  padding-top: .75rem;
+  padding-bottom: .75rem;
+  font-size: 1rem;
+  background-color: rgba(0, 0, 0, .25);
+  box-shadow: inset -1px 0 0 rgba(0, 0, 0, .25);
+}
+
+.navbar .form-control {
+  padding: .75rem 1rem;
+  border-width: 0;
+  border-radius: 0;
+}
+
+.form-control-dark {
+  color: #fff;
+  background-color: rgba(255, 255, 255, .1);
+  border-color: rgba(255, 255, 255, .1);
+}
+
+.form-control-dark:focus {
+  border-color: transparent;
+  box-shadow: 0 0 0 3px rgba(255, 255, 255, .25);
+}
+
+</style>
