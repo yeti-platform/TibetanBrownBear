acebfcb0a3c953d8d88916e2609ad35d3ad6e27b
Thomas Chopitea <tomchop@gmail.com>
2018-04-16 18:38:34 +0200

    Tags API

--- yeti/web/api/api.py
+++ yeti/web/api/api.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 from .observable import ObservableResource
 from .entity import EntityResource
 from .indicator import IndicatorResource
+from .tag import TagResource
 
 
 blueprint = Blueprint('api', __name__)
@@ -12,3 +13,4 @@ blueprint = Blueprint('api', __name__)
 ObservableResource.register(blueprint)
 EntityResource.register(blueprint)
 IndicatorResource.register(blueprint)
+TagResource.register(blueprint)
---
+++ yeti/web/api/tag.py
@@ -0,0 +1,16 @@
+"""API resources for interacting with Tags."""
+from marshmallow import fields
+
+from yeti.core.types.tag import Tag
+from .generic import GenericResource
+
+
+class TagResource(GenericResource):
+    """Class describing resources to manipulate Entity objects."""
+
+    route_base = '/tags/'
+    resource_object = Tag
+
+    searchargs = {
+        'name': fields.Str(required=True),
+    }
