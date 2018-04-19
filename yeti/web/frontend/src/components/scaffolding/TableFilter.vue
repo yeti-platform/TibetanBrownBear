667783b76da369f64c406048701d65852556bb49
Thomas Chopitea <tomchop@gmail.com>
2018-04-19 18:14:32 +0200

    TableFilter tracks selected objects

--- yeti/web/frontend/src/components/scaffolding/TableFilter.vue
+++ yeti/web/frontend/src/components/scaffolding/TableFilter.vue
@@ -5,12 +5,16 @@
       <div v-if="loading">
         Loading...
       </div>
-      <table v-else class="table table-hover table-compact table-sm">
+      <table v-else class="table table-hover table-compact table-sm table-yeti">
         <thead>
           <tr><th v-bind:key="field" v-for="field in filterParams.fields">{{field}}</th></tr>
         </thead>
         <tbody>
-          <tr v-for="elt in elements" v-bind:key="elt.id">
+          <tr v-for="elt in elements"
+              v-bind:key="elt.id"
+              @click.exact="select(elt)"
+              @click.shift.exact="selectMultiple(elt)"
+              v-bind:class="{'selected': selectedElements.includes(elt.id)}">
             <td v-bind:key="field" v-for="(field, index) in filterParams.fields">
               <router-link v-if="index === 0" :to="{ name: detailComponent, params: {id: elt.id}}">
                 <fields :field="field" :value="elt[field]"/>
@@ -32,12 +36,13 @@ export default {
   components: {
     Fields
   },
-  props: ['filterParams', 'detailComponent'],
+  props: ['input', 'filterParams', 'detailComponent'],
   data () {
     return {
       elements: [],
       searchQuery: '',
-      loading: true
+      loading: true,
+      selectedElements: []
     }
   },
   watch: {
@@ -52,7 +57,9 @@ export default {
       params['type'] = this.filterParams.typeFilter
       axios.post(this.filterParams.apiPath, params)
         .then(response => {
-          this.elements = response.data
+          this.elements = response.data.map(function (elt) {
+            elt.selected = false; return elt
+          })
           this.loading = false
         })
         .catch(error => {
@@ -61,6 +68,18 @@ export default {
     },
     getElements () {
       this.fetchElements()
+    },
+    select (elt) {
+      this.selectedElements = [elt.id]
+      this.$emit('input', [elt])
+    },
+    selectMultiple (elt, event) {
+      if (!this.selectedElements.includes(elt.id)) {
+        this.selectedElements.push(elt.id)
+      } else {
+        this.selectedElements.splice(this.selectedElements.indexOf(elt.id), 1)
+      }
+      this.$emit('input', this.elements.filter(elt => this.selectedElements.includes(elt.id)))
     }
   },
   created () {
@@ -68,3 +87,16 @@ export default {
   }
 }
 </script>
+
+<style lang="css">
+.selected {
+  font-weight: bold;
+}
+
+.table-yeti {
+            user-select: none; /* CSS3 (little to no support) */
+        -ms-user-select: none; /* IE 10+ */
+       -moz-user-select: none; /* Gecko (Firefox) */
+    -webkit-user-select: none; /* Webkit (Safari, Chrome) */
+}
+</style>
