bcc4c63b6cecedc39e774db7af12c1b8e7ef53d8
Thomas Chopitea <tomchop@gmail.com>
2018-05-10 12:06:27 +0200

    Fix warnings when running tests

--- yeti/web/frontend/test/unit/specs/Sidebar.spec.js
+++ yeti/web/frontend/test/unit/specs/Sidebar.spec.js
@@ -1,8 +1,22 @@
-import { mount } from '@vue/test-utils'
+import { createLocalVue, mount } from '@vue/test-utils'
 import Sidebar from '@/components/scaffolding/Sidebar'
+import Router from 'vue-router'
+import { routes } from '@/router'
 
 describe('Sidebar.vue', () => {
-  let wrapper = mount(Sidebar)
+  let wrapper
+  let localVue
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(Router)
+    let router = new Router({routes: routes, mode: 'history'})
+    wrapper = mount(Sidebar, {
+      localVue,
+      router,
+      propsData: { id: 510808 }
+    })
+  })
 
   it('should render correct contents', () => {
     expect(wrapper.vm.$el.querySelector('span').textContent)
