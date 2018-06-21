935596e174782c0f396cf3c9a2438148232c2061
Thomas Chopitea <tomchop@gmail.com>
2018-06-21 19:19:45 +0200

    Access the stix object through properties

--- tests/stix2/entities.py
+++ tests/stix2/entities.py
@@ -33,27 +33,27 @@ def test_update_malware():
     """Tests that a Malware object is succesfully updated."""
     malware = Malware(name='asd', labels=['label1'])
     malware.save()
-    stix_id = malware._stix_object.id
+    stix_id = malware.id
     updated = malware.update({'name': 'dsa'})
-    assert updated._stix_object.name == 'dsa'
-    assert updated._stix_object.id == stix_id
+    assert updated.name == 'dsa'
+    assert updated.id == stix_id
 
 @pytest.mark.usefixtures('clean_db')
 def test_malware_versionning():
     """Tests that a getting a Malware object returns the most recent version."""
     malware = Malware(name='asd', labels=['label1'])
     malware.save()
-    stix_id = malware._stix_object.id
+    stix_id = malware.id
     malware.update({'name': 'dsa'})
     fetched = Malware.get(stix_id)
-    assert fetched._stix_object.id == stix_id
-    assert fetched._stix_object.created < fetched._stix_object.modified
+    assert fetched.id == stix_id
+    assert fetched.created < fetched.modified
 
 @pytest.mark.usefixtures('clean_db')
 def test_all_versions():
     """Tests that a updating malware results in two versions."""
     malware = Malware(name='asd', labels=['label1'])
     malware.save()
-    stix_id = malware._stix_object.id
+    stix_id = malware.id
     malware.update({'name': 'dsa'})
     assert len(malware.all_versions()) == 2
--- yeti/core/entities/stix_entity.py
+++ yeti/core/entities/stix_entity.py
@@ -1,13 +1,11 @@
 """Detail Yeti's Entity object structure."""
 import json
 
-from dateutil import parser
-from marshmallow import fields, post_load
 from stix2 import parse
 from stix2.exceptions import MissingPropertiesError, ParseError
 
-from yeti.core.errors import ValidationError
-from ..model.database import YetiObject, YetiSchema
+from yeti.core.errors import ValidationError, IntegrityError
+from ..model.database import YetiObject
 
 class Entity(YetiObject):
     """Entity Yeti object.
@@ -26,17 +24,53 @@ class Entity(YetiObject):
     ]
 
     def _stix_parse(self, stix_dict):
+        """Parses a dictionary into an actual STIX2 object.
+
+        Args:
+          stix_dict: The STIX dictionary to use to create the STIX obejct.
+
+        Raises:
+          ValidationError: If the dictionary does not comply with the STIX2
+              standard.
+        """
         stix_dict['type'] = self.stix_type
         try:
             self._stix_object = parse(stix_dict)
         except (MissingPropertiesError, ParseError) as err:
             raise ValidationError(err)
 
+    # We don't want to call YetiObject's init method since this would set all
+    # attributes in stix_dict in the Entity object; we want them in
+    # _stix_object.
+    # pylint: disable=super-init-not-called
     def __init__(self, **stix_dict):
+        """Initializes an Entity's STIX object.
+
+        Args:
+          stix_dict: The dictionary to use to create the STIX object.
+        """
+        self._stix_object = None
         self._stix_parse(stix_dict)
 
+    # We want to call this argument stix_dict instead of the generic "args" in
+    # YetiObject.
+    # pylint: disable=arguments-differ
     @classmethod
-    def load(cls, stix_dict, strict=False):
+    def load(cls, stix_dict):
+        """Translate information from the backend into a valid STIX definition.
+
+        Will instantiate a STIX object from that definition.
+
+        Args:
+          stix_dict: The dictionary to use to create the STIX object.
+
+        Returns:
+          The corresponding STIX objet.
+
+        Raises:
+          IntegrityError: If a STIX object could not be instantiated from the
+              data in the database.
+        """
         stix_dict.pop('_key')
         stix_dict.pop('_id')
         stix_dict.pop('_rev')
@@ -44,37 +78,96 @@ class Entity(YetiObject):
         try:
             return cls(**stix_dict)
         except Exception as err:
-            raise ValidationError(err)
+            raise IntegrityError(err)
 
     def dump(self):
+        """Dumps an Entity object into it's STIX JSON representation.
+
+        Returns:
+          The Entity's JSON representation in dictionary form.
+        """
         serialized = json.loads(self._stix_object.serialize())
         serialized['stix_id'] = serialized['id']
         serialized['id'] = None
         return serialized
 
     def all_versions(self):
-        return self.filter({'stix_id': self._stix_object.id})
+        """Returns all versions of a STIX object given its key.
+
+        Returns:
+          A list of STIX objects.
+        """
+        return self.filter({'stix_id': self.id})
 
     @classmethod
     def get(cls, key):
+        """Fetches the most recent version of a STIX object given its key.
+
+        Returns:
+          A STIX object.
+        """
         all_versions = cls.filter({'stix_id': key})
-        modified = all_versions[0]._stix_object.modified
+        modified = all_versions[0].modified
         winner = all_versions[0]
         for version in all_versions:
-            parsed_timestamp = version._stix_object.modified
+            parsed_timestamp = version.modified
             if parsed_timestamp > modified:
                 modified = parsed_timestamp
                 winner = version
         return winner
 
     def update(self, args):
+        """Updates a STIX object, creating a new version.
+
+        Args:
+          args: a {key:value} dicionary containing attributes to update.
+
+        Returns:
+          The new version of the STIX object.
+        """
         new_version = self._stix_object.new_version(**args)
         self._stix_object = new_version
         return self.save()
 
-    # def save(self):
-    #     return YetiObject.save(self)
-
-
     def __repr__(self):
         return str(self._stix_object)
+
+    @property
+    def type(self):
+        return self._stix_object.type
+
+    @property
+    def id(self):
+        return self._stix_object.id
+
+    @property
+    def created_by_ref(self):
+        return self._stix_object.created_by_ref
+
+    @property
+    def created(self):
+        return self._stix_object.created
+
+    @property
+    def modified(self):
+        return self._stix_object.modified
+
+    @property
+    def revoked(self):
+        return self._stix_object.revoked
+
+    @property
+    def labels(self):
+        return self._stix_object.labels
+
+    @property
+    def external_references(self):
+        return self._stix_object.external_references
+
+    @property
+    def object_marking_refs(self):
+        return self._stix_object.object_marking_refs
+
+    @property
+    def granular_markings(self):
+        return self._stix_object.granular_markings
