1b7ea1ce99ef4b1b1292d5b69fe3bb5196fc26cd
Thomas Chopitea <tomchop@gmail.com>
2018-06-24 15:17:21 +0200

    Fix validation error

--- yeti/core/entities/entity.py
+++ yeti/core/entities/entity.py
@@ -1,6 +1,7 @@
 """Detail Yeti's Entity object structure."""
 
 from yeti.core.stix_sdo import StixSDO
+from yeti.core.errors import ValidationError
 
 class Entity(StixSDO):
 
@@ -18,5 +19,9 @@ class Entity(StixSDO):
     def get_final_datatype(cls, args):
         subclass = cls
         if 'type' in args:
+            if args['type'] not in cls.datatypes:
+                raise ValidationError(
+                    '"{0:s}" not in acceptable datatypes ({1!r})'.format(
+                        args['type'], cls.datatypes))
             subclass = cls.datatypes[args['type']]
         return subclass
