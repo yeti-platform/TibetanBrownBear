9839c2461da95bce17267b42ee9becd353c3bbab
Thomas Chopitea <tomchop@gmail.com>
2018-07-13 21:51:14 +0200

    Better json helper structure

--- yeti/web/helpers.py
+++ yeti/web/helpers.py
@@ -6,27 +6,27 @@ from yeti.core.errors import GenericYetiError
 from yeti.core.model.database import YetiObject
 
 def _as_json(result): # pylint: disable=too-many-return-statements
-    if isinstance(result, tuple):
-        response, code = result
-        if isinstance(response, GenericYetiError):
-            return jsonify({response.type: response.message}), code
-        return jsonify(response), code
     if isinstance(result, list):
         if not result:
-            return jsonify([])
-        if isinstance(result[0], YetiObject):
-            return jsonify([item.dump(destination='web') for item in result])
-        if isinstance(result[0], dict):
-            return jsonify(result)
+            return []
+        return [_as_json(item) for item in result]
+    if isinstance(result, dict):
+        return {key: _as_json(value) for key, value in result.items()}
     if isinstance(result, YetiObject):
-        return jsonify(result.dump(destination='web'))
-    return jsonify(result)
+        return result.dump(destination='web')
+    return result
 
 def as_json(func):
     """Attempts to jsonify YetiObjects and lists of YetiObjects."""
     @functools.wraps(func)
     def inner(*args, **kwargs):
-        return _as_json(func(*args, **kwargs))
+        json_response = _as_json(func(*args, **kwargs))
+        if isinstance(json_response, tuple):
+            response, code = json_response
+            if isinstance(response, GenericYetiError):
+                return jsonify({response.type: response.message}), code
+            return jsonify(response), code
+        return jsonify(json_response)
     return inner
 
 def get_object_or_404(klass, key):
