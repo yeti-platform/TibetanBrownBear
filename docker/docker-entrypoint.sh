b74c40aabd1e62a65d1d1899910093e3174fa016
Thomas Chopitea <tomchop@gmail.com>
2018-09-20 16:17:21 +0200

    Adjust docker entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,7 +2,11 @@
 set -e
 
 if [ "$1" = 'webserver' ]; then
-    exec python3 /opt/yeti/yetictl webserver
+    exec python3 /opt/yeti/cli/yetictl webserver
+fi
+
+if [ "$1" = 'taxii' ]; then
+    exec python3 /opt/yeti/cli/yetictl taxii_import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
 fi
 
 exec "$@"
