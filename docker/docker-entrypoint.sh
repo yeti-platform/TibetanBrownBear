1f35ba709b3a1b1bff95e1e5a3e32096cd4b0b09
Thomas Chopitea <tomchop@gmail.com>
2018-09-24 17:15:31 +0200

    Docker entrypoint udpate

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,11 +2,11 @@
 set -e
 
 if [ "$1" = 'webserver' ]; then
-    exec python3 /opt/yeti/cli/yetictl.py webserver
+    exec yeticli webserver
 fi
 
 if [ "$1" = 'taxii' ]; then
-    exec python3 /opt/yeti/cli/yetictl.py taxii_import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
+    exec yeticli taxii_import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
 fi
 
 exec "$@"
