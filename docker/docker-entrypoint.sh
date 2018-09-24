f51382ad661f02c315a72f987e5c21c03a37092a
Thomas Chopitea <tomchop@gmail.com>
2018-09-24 17:45:10 +0200

    try pythonpath

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+PYTHONPATH="/opt/yeti"
+
 if [ "$1" = 'webserver' ]; then
-    exec yeticli webserver
+    exec python3 /opt/yeti/cli/yetictl.py webserver
 fi
 
 if [ "$1" = 'taxii' ]; then
-    exec yeticli taxii_import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
+    exec python3 /opt/yeti/cli/yetictl.py taxii_import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
 fi
 
 exec "$@"
