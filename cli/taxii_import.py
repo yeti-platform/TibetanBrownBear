2ac4b33f2cf2700686aefff3e35399ca961222ca
Thomas Chopitea <tomchop@gmail.com>
2018-10-15 18:22:51 +0200

    List TAXII server collections (closes #102)

--- cli/taxii_import.py
+++ cli/taxii_import.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env
 import click
 from stix2 import TAXIICollectionSource, Filter
-from taxii2client import Collection
+from taxii2client import Collection, Server
 
 from yeti.core.entities import attack_pattern
 from yeti.core.entities import campaign
@@ -14,56 +14,78 @@ from yeti.core.entities import tool
 from yeti.core.entities import vulnerability
 
 @click.command()
-@click.option('--collection_url', help='Remote TAXII collection URL ', type=click.STRING, required=True)  # pylint: disable=line-too-long
-def taxii_import(collection_url):
-    print('Importing data from collection at: {0:s}')
-    collection = Collection(collection_url)
-    tc_source = TAXIICollectionSource(collection)
+@click.option('--collection_url', help='Remote TAXII collection URL ', type=click.STRING)  # pylint: disable=line-too-long
+@click.option('--server_url', help='Remote TAXII collection URL ', type=click.STRING)  # pylint: disable=line-too-long
+def taxii_import(server_url, collection_url):
 
-    object_classes = {
-        'attack-pattern': attack_pattern.AttackPattern,
-        'campaign': campaign.Campaign,
-        'course-of-action': course_of_action.CourseOfAction,
-        'identity': identity.Identity,
-        'intrusion-set': intrusion_set.IntrusionSet,
-        'malware': malware.Malware,
-        'threat-actor': threat_actor.ThreatActor,
-        'tool': tool.Tool,
-        'vulnerability': vulnerability.Vulnerability,
-    }
+    if (not server_url or collection_url):
+        print('Please specify one of --server_url or --collection_url')
+        exit(-1)
 
-    all_objects = {}
+    if server_url:
+        server = Server(server_url)
+        api_root = server.api_roots[0]
+        collections = list(api_root.collections)
+        print('{0:s} has {1:d} collections: '.format(server_url, len(collections)))
+        for index, collection in enumerate(collections):
+            print("[{0:d}] {1:s}: {2:s}".format(index, collection.title, collection.id))
+        collection_url = None
+        while not collection_url:
+            choice = input('Pick one: ')
+            try:
+                collection_url = collections[int(choice)].url
+            except (ValueError, IndexError):
+                print('Please specify a number from 0 to {0:d}'.format(len(collections)))
 
-    for name, yeti_class in object_classes.items():
-        print('Fetching', name)
-        stats = {
-            'updated': 0,
-            'new': 0,
-            'skipped': 0,
+    if collection_url:
+        print('Importing data from collection at: {0:s}'.format(collection_url))
+        collection = Collection(collection_url)
+        tc_source = TAXIICollectionSource(collection)
+
+        object_classes = {
+            'attack-pattern': attack_pattern.AttackPattern,
+            'campaign': campaign.Campaign,
+            'course-of-action': course_of_action.CourseOfAction,
+            'identity': identity.Identity,
+            'intrusion-set': intrusion_set.IntrusionSet,
+            'malware': malware.Malware,
+            'threat-actor': threat_actor.ThreatActor,
+            'tool': tool.Tool,
+            'vulnerability': vulnerability.Vulnerability,
         }
 
-        items = tc_source.query(Filter('type', '=', name))
-        for item in items:
-            obj = yeti_class.find(stix_id=item['id'])
-            if not obj:
-                obj = yeti_class.from_stix_object(item).save()
-                stats['new'] += 1
-            elif obj.equals(item):
-                stats['skipped'] += 1
-            else:
-                obj.update(item)
-                stats['updated'] += 1
-            all_objects[item['id']] = obj
+        all_objects = {}
 
-        print('New: {0:d}, Updated: {1:d}, Skipped: {2:d}'.format(
-            stats['new'], stats['updated'], stats['skipped']
-        ))
+        for name, yeti_class in object_classes.items():
+            print('Fetching', name)
+            stats = {
+                'updated': 0,
+                'new': 0,
+                'skipped': 0,
+            }
 
-    print('Getting relationships')
-    stats = 0
-    for relationship in tc_source.query(Filter('type', '=', 'relationship')):
-        stats += 1
-        source = all_objects[relationship.source_ref]
-        target = all_objects[relationship.target_ref]
-        source.link_to(target, stix_rel=relationship.serialize())
-    print('Added {0:d} relationships'.format(stats))
+            items = tc_source.query(Filter('type', '=', name))
+            for item in items:
+                obj = yeti_class.find(stix_id=item['id'])
+                if not obj:
+                    obj = yeti_class.from_stix_object(item).save()
+                    stats['new'] += 1
+                elif obj.equals(item):
+                    stats['skipped'] += 1
+                else:
+                    obj.update(item)
+                    stats['updated'] += 1
+                all_objects[item['id']] = obj
+
+            print('New: {0:d}, Updated: {1:d}, Skipped: {2:d}'.format(
+                stats['new'], stats['updated'], stats['skipped']
+            ))
+
+        print('Getting relationships')
+        stats = 0
+        for relationship in tc_source.query(Filter('type', '=', 'relationship')):
+            stats += 1
+            source = all_objects[relationship.source_ref]
+            target = all_objects[relationship.target_ref]
+            source.link_to(target, stix_rel=relationship.serialize())
+        print('Added {0:d} relationships'.format(stats))
