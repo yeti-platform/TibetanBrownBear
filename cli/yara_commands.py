18901a49eb5a1b5be56fd3f60d23f67c00567eda
Thomas Chopitea <tomchop@gmail.com>
2018-10-23 14:03:08 +0200

    Yara matching command

--- cli/yara_commands.py
+++ cli/yara_commands.py
@@ -3,7 +3,6 @@
 import os
 
 import click
-import yara
 
 from yeti_python_api import api
 
@@ -18,20 +17,43 @@ def yara_scan(path, name_filter, verbose, recurse):
         print('Error: {0:s} was not found'.format(path))
         exit(-1)
     yara_rules = api.filter_indicators(name_filter, 'x-yara')
+
+    paths = [path]
+    max_path_len = 0
+    if recurse:
+        print('Recursing on directory {0:s}'.format(path))
+        paths = []
+        for root, _, files in os.walk(path):
+            for filename in files:
+                full_path = os.path.join(root, filename)
+                paths.append(full_path)
+                if len(full_path) > max_path_len:
+                    max_path_len = len(full_path)
+
+    results = []
+    max_ruleid_len = 0
     for rule in yara_rules:
-        results = rule.compiled_pattern.match(path)
-        if not results:
-            print('No matches found.')
-            exit()
-        print('Found {0:d} matches!'.format(len(results)))
-        print('{0:<20s} {1:<55s} {2:s}'.format(
-            'Name', 'ID', 'Details' if verbose else ''))
-        print('{0:<20s} {1:<55s} {2:s}'.format(
-            '='*10, '='*10, '='*10 if verbose else ''))
-        for result in results:
-            print('{0:<20s} {1:<55s} {2!s}'.format(
-                rule.name,
-                rule.id,
+        for filename in paths:
+            matches = rule.compiled_pattern.match(filename)
+            if matches:
+                if len(rule.name) > max_ruleid_len:
+                    max_ruleid_len = len(rule.name + rule.id)
+                results.append((filename, rule, matches))
+
+    if not results:
+        print('No matches found')
+        exit()
+
+    print('Found {0:d} matches!'.format(len(results)))
+    print('{0:s}   {1:s}   {2:s}'.format(
+        'Filename'.ljust(max_path_len), 'ID'.ljust(max_ruleid_len + 3), 'Details' if verbose else ''))
+    print('{0:s}   {1:s}   {2:s}'.format(
+        '='.ljust(max_path_len, '='), '='.ljust(max_ruleid_len + 3, '='), '='*10 if verbose else ''))
+    for filename, rule, result_list in results:
+        for result in result_list:
+            print('{0:s}   {1:s}   {2!s}'.format(
+                filename.ljust(max_path_len),
+                '{} ({})'.format(rule.name, rule.id).ljust(max_ruleid_len),
                 result.strings if verbose else ''
             ))
 
