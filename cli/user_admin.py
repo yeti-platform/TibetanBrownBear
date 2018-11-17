702e7e954ed047e253da338b6d2d14068fd8fb5c
Thomas Chopitea <tomchop@gmail.com>
2018-11-17 22:06:17 +0100

    Add admin option

--- cli/user_admin.py
+++ cli/user_admin.py
@@ -7,9 +7,12 @@ from yeti.core.model.user import User
 # pylint: disable=line-too-long
 @click.command()
 @click.option('--password', prompt='Password', hide_input=True, confirmation_prompt=True, help='Take password from CLI', type=click.STRING)
+@click.option('--admin', help='Give admin rights to this user', is_flag=True, default=False)
 @click.argument('user_email')
-def add_user(user_email, password=None):
-    user = User(email=user_email).save()
+def add_user(user_email, password=None, admin=False):
+    user = User(email=user_email, admin=admin).save()
     user_management.set_password(user, password)
     user.save()
     print(f'User {user_email} created succesfully (ID: {user.id})')
+    print(f'Admin: {user.admin}')
+    print(f'API key: {user.api_key}')
