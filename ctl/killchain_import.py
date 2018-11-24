16eaaebc6ad5580470844ed8d104a121d42180e7
Thomas Chopitea <tomchop@gmail.com>
2018-11-25 00:01:45 +0100

    Add MITRE tactics import

--- ctl/killchain_import.py
+++ ctl/killchain_import.py
@@ -3,6 +3,9 @@ import json
 import os
 import click
 
+from taxii2client import Collection
+from stix2 import TAXIICollectionSource, Filter
+
 from yeti.core.model.settings import setting
 from yeti.core.model.settings.killchains import KillChains
 
@@ -18,3 +21,18 @@ def killchain_import(killchain_filter):
             phases = killchain.pop('phases')
             kc = KillChains(**killchain)
             kc.set_phases(phases)
+
+MITRE_TACTICS_URL = "https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/"
+@click.command()
+def mitre_tactics_import():
+    collection = Collection(MITRE_TACTICS_URL)
+    tc_source = TAXIICollectionSource(collection)
+    kc = KillChains.get_or_create(name='mitre-attack')
+    kc.description = 'The MITRE ATT&CK tactics are represented as kill-chains in STIX2'
+    kc.save()
+    for item in tc_source.query(Filter('type', '=', 'x-mitre-tactic')):
+        print(f'Adding {item["x_mitre_shortname"]}')
+        kc.add_phase_to_killchain({
+            'name': item['x_mitre_shortname'],
+            'description': item['description']
+        })
