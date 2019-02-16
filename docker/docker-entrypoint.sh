b99c712068991f0965d04b1f737cb31d12e5e49c
Thomas Chopitea <tomchop@gmail.com>
2019-02-16 22:32:36 +0100

    Entrypoint tweaks

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,12 +3,17 @@ set -e
 
 PYTHONPATH="/opt/yeti"
 
+echo '### Yeti docker entrypoint ###'
+echo -n 'Command: '
+echo $1
+
 if [ "$1" = 'webserver' ]; then
     exec python3 /opt/yeti/ctl/yetictl.py webserver
 fi
 
-if [ "$1" = 'taxii' ]; then
+if [ "$1" = 'taxii-mitre' ]; then
     exec python3 /opt/yeti/ctl/yetictl.py taxii-import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
 fi
 
-exec "$@"
+echo "Command $@ not found."
+exec python3 /opt/yeti/ctl/yetictl.py "$@"
