30d12adf576bb260dfc9083a77316a13f57b7db7
Thomas Chopitea <tomchop@gmail.com>
2019-02-17 17:22:24 +0100

    Update entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,15 +3,17 @@ set -e
 
 PYTHONPATH="/opt/yeti"
 
-if [ "$1" =~ "bootstrap" ]; then
+if [[ "$1" =~ "bootstrap-webserver" ]]; then
     python3 /opt/yeti/ctl/yetictl.py taxii-import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
     python3 /opt/yeti/ctl/yetictl.py mitre-taxii-import
     python3 /opt/yeti/ctl/yetictl.py vocab-import
     python3 /opt/yeti/ctl/yetictl.py killchain-import
     python3 /opt/yeti/ctl/yetictl.py add-user admin@admin.com
+    exec python3 /opt/yeti/ctl/yetictl.py webserver
+
 fi
 
-if [ "$1" =~ "webserver" ]; then
+if [[ "$1" =~ "webserver" ]]; then
     exec python3 /opt/yeti/ctl/yetictl.py webserver
 fi
 
