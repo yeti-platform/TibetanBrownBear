24b54bcd10f3d0a8a3026195022257afe4812865
Thomas Chopitea <tomchop@gmail.com>
2019-02-17 18:53:33 +0100

    bugfix

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,7 +5,7 @@ PYTHONPATH="/opt/yeti"
 
 if [[ "$1" =~ "bootstrap-webserver" ]]; then
     python3 /opt/yeti/ctl/yetictl.py taxii-import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
-    python3 /opt/yeti/ctl/yetictl.py mitre-taxii-import
+    python3 /opt/yeti/ctl/yetictl.py mitre-tactics-import
     python3 /opt/yeti/ctl/yetictl.py vocab-import
     python3 /opt/yeti/ctl/yetictl.py killchain-import
     python3 /opt/yeti/ctl/yetictl.py add-user admin@admin.com
