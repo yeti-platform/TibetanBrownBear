90aacd83f9383513b438f778564eb1a967533212
Thomas Chopitea <tomchop@gmail.com>
2019-02-17 15:35:22 +0100

    Change stuff

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -3,17 +3,16 @@ set -e
 
 PYTHONPATH="/opt/yeti"
 
-echo '### Yeti docker entrypoint ###'
-echo -n 'Command: '
-echo $1
+if [ "$1" =~ "bootstrap" ]; then
+    python3 /opt/yeti/ctl/yetictl.py taxii-import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
+    python3 /opt/yeti/ctl/yetictl.py mitre-taxii-import
+    python3 /opt/yeti/ctl/yetictl.py vocab-import
+    python3 /opt/yeti/ctl/yetictl.py killchain-import
+    python3 /opt/yeti/ctl/yetictl.py add-user admin@admin.com
+fi
 
-if [ "$1" = 'webserver' ]; then
+if [ "$1" =~ "webserver" ]; then
     exec python3 /opt/yeti/ctl/yetictl.py webserver
 fi
 
-if [ "$1" = 'taxii-mitre' ]; then
-    exec python3 /opt/yeti/ctl/yetictl.py taxii-import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
-fi
-
-echo "Command $@ not found."
-exec python3 /opt/yeti/ctl/yetictl.py "$@"
+exec "$@"
