70643eec176f6726b0dc67e933e31e16b480426d
Thomas Chopitea <tomchop@gmail.com>
2020-01-19 18:31:27 +0100

    Import all MITRE sources

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 PYTHONPATH="/opt/yeti"
 
 if [[ "$1" =~ "bootstrap-webserver" ]]; then
+    python3 /opt/yeti/ctl/yetictl.py taxii-import --collection_url https://cti-taxii.mitre.org/stix/collections/2f669986-b40b-4423-b720-4396ca6a462b/
+    python3 /opt/yeti/ctl/yetictl.py taxii-import --collection_url https://cti-taxii.mitre.org/stix/collections/062767bd-02d2-4b72-84ba-56caef0f8658/
     python3 /opt/yeti/ctl/yetictl.py taxii-import --collection_url https://cti-taxii.mitre.org/stix/collections/95ecc380-afe9-11e4-9b6c-751b66dd541e/
     python3 /opt/yeti/ctl/yetictl.py mitre-tactics-import
     python3 /opt/yeti/ctl/yetictl.py vocab-import
