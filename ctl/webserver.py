c5f16f0c52e6f6b2832927a9b04705fbc44ff7d3
Thomas Chopitea <tomchop@gmail.com>
2020-04-24 19:48:01 +0200

    Tweak debug option

--- ctl/webserver.py
+++ ctl/webserver.py
@@ -5,7 +5,7 @@ from yeti.common.config import yeti_config
 
 # pylint: disable=line-too-long
 @click.command()
-@click.option('--debug', is_flag=True, help='launch server in debug mode', default=yeti_config.webserver.debug, type=click.BOOL)
+@click.option('--debug/--no-debug', is_flag=True, help='launch server in debug mode', default=yeti_config.webserver.debug)
 @click.option('--interface', help='interface to listen on', default=yeti_config.webserver.interface, type=click.STRING)
 @click.option('--port', help='port to listen on', default=yeti_config.webserver.port, type=click.INT)
 def webserver(debug, interface, port):
